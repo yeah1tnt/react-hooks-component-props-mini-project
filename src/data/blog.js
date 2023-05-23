4ac409cb541c6a6abf42bd53be07f9842b4dee37
Tong Tang <pichip22@hotmail.com>
2023-05-23 18:59:53 -0400

    completed

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,8 +1,8 @@
-import logo from "../assets/logo.svg";
+//import logo from "../assets/logo.svg";
 
 const blogData = {
   name: "Underreacted",
-  image: logo,
+  image: "",
   about: "A blog about learning React",
   posts: [
     {
